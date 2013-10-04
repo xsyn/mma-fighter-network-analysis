2739a188b3ce00372c9ca84ec6e5d150de280291
tokestermw <tokestermw@gmail.com>
2013-10-04 07:42:41 -0700

    rewriting in scrapey

--- sherdog/sherdog/items.py
+++ sherdog/sherdog/items.py
@@ -5,9 +5,38 @@
 
 from scrapy.item import Item, Field
 
-class SherdogItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
+class FightItem(Item):
+    '''
+    Should have all the fights with ID of "Fight N".
+    There should be a FightItem() call for each fight.
+    Dictionaries of all fights are then put in FighterItem().
+    '''
     Verdict = Field()
     Opponent = Field()
+    Event = Field()
+    Date = Field()
+    Method = Field()
+    Round = Field()
+    Time = Field()
+    pass
+
+class AttrItem(Item):
+    '''
+    Fighters attribute, one for each fighter.
+    Dictionaries alongside dictionaries of fights.
+    '''
+    Birthday = Field()
+    Weight = Field()
+    Height = Field()
+    Class = Field()
+    Country = Field()
+    pass
+
+class FighterItem(Item):
+    '''
+    This is a superclass of FightItem and AttrItem so it binds both of them via
+    dictionaries.
+    '''
+    Fight = Field()
+    Attr = Field()
     pass
--- sherdog/sherdog/spiders/sherdogspider.py
+++ sherdog/sherdog/spiders/sherdogspider.py
@@ -1,4 +1,35 @@
 from scrapy.spider 	import BaseSpider
-from scrapy.selector 	import HtmlXPathSelector
-from sherdog.items	import SherdogItem
+from scrapy.selector import HtmlXPathSelector
+from sherdog.items	import FightItem, AttrItem, FighterItem
 from scrapy.http	import Request
+import datetime as dt
+import re
+
+class SherdogSpider(BaseSpider):
+    name = "sherdog"
+    allowed_domains = ["sherdog.com"]
+    start_urls = ["http://www.sherdog.com/fighter/Mauricio-Rua-5707"]
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        tabs = hxs.select("//tr[@class='even']")
+        i = 1
+        for tab in reversed(tabs):
+            FI = FightItem()
+            FI["Verdict"] = tab.select("./td")
+            FI["Opponent"]
+            FI["Event"]
+            FI["Date"]
+            FI["Method"]
+            FI["Round"]
+            FI["Time"]
+            Fighter["Fight" + str(i)] = FI
+            i += 1
+        bio = hxs.select("//div[@class='bio']")
+        AI = AttrItem()
+        AI["Birthday"] = dt.datetime.strptime(bio.select(".//span[@itemprop='birthDate']/text()")[0].extract(), "%Y-%m-%d")
+        AI["Weight"] = int(bio.select(".//span[@class='item weight']//strong/text()").extract()[0].split()[0])
+        AI["Height"] = sum(int(x) * 12 ** k for k, x in enumerate(reversed(bio.select(".//span[@class='item height']/strong/text()").extract()[0].split("\"")[0].split("\'"))))
+        AI["Class"] = bio.select(".//strong[@class='title']/text()").extract()[0]
+        AI["Country"] = bio.select(".//strong[@itemprop='nationality']/text()").extract()[0]
+        Fighter["Bio"] = AI
