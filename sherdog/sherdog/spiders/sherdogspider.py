e01a6e65814573028d8b58b193df454d5b135613
Guy Taylor <xsyn@Sapientia.local>
2015-01-21 16:18:41 +0200

    Some basic debugging to handle missing data in Birthdays and Fight times

--- sherdog/sherdog/spiders/sherdogspider.py
+++ sherdog/sherdog/spiders/sherdogspider.py
@@ -35,7 +35,10 @@ class SherdogSpider(CrawlSpider):
             FI["Date"] = dt.datetime.strptime(tab.select(".//span[@class='sub_line']/text()")[0].extract(), "%b / %d / %Y")
             FI["Method"] = tab.select(".//td/text()")[0].extract()
             FI["Round"] = int(tab.select(".//td/text()")[1].extract())
-            FI["Time"] = sum(int(x) * 60 **k for k, x in enumerate(reversed(tab.select(".//td/text()")[2].extract().split(":"))))
+            if (tab.select(".//td/text()")[2].extract()) != "N/A":
+                FI["Time"] = sum(int(x) * 60 **k for k, x in enumerate(reversed(tab.select(".//td/text()")[2].extract().split(":"))))
+            else:
+                FI["Time"] = 0
             Fights["Fight" + str(i)] = dict(FI)
             i += 1
 
@@ -43,7 +46,10 @@ class SherdogSpider(CrawlSpider):
         bio = hxs.select("//div[@class='bio']")
         AI = AttrItem()
         AI["Name"] = hxs.select(".//span[@class='fn']/text()")[0].extract()
-        AI["Birthday"] = dt.datetime.strptime(bio.select(".//span[@itemprop='birthDate']/text()")[0].extract(), "%Y-%m-%d")
+        if bio.select(".//span[@itemprop='birthDate']/text()")[0].extract() != "N/A":
+            AI["Birthday"] = dt.datetime.strptime(bio.select(".//span[@itemprop='birthDate']/text()")[0].extract(), "%Y-%m-%d")
+        else:
+             AI["Birthday"] == "Unknown"
         AI["Weight"] = int(bio.select(".//span[@class='item weight']//strong/text()").extract()[0].split()[0])
         AI["Height"] = sum(int(x) * 12 ** k for k, x in enumerate(reversed(bio.select(".//span[@class='item height']/strong/text()").extract()[0].split("\"")[0].split("\'"))))
         AI["Class"] = bio.select(".//strong[@class='title']/text()").extract()[0]
@@ -55,5 +61,5 @@ class SherdogSpider(CrawlSpider):
         Fighter["Bio"] = dict(AI)
 
         log.msg('Currently getting ' + AI["Name"], level = log.WARNING)
-        
+
         return Fighter
