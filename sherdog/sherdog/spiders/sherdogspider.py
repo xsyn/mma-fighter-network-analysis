75f312baf5cb370df82487ffc3b32264814b6ea5
xsyn <guy.taylor@gmail.com>
2015-01-22 13:32:44 +0200

    moving from deprecated .select() to .xpath()

--- sherdog/sherdog/spiders/sherdogspider.py
+++ sherdog/sherdog/spiders/sherdogspider.py
@@ -33,39 +33,40 @@ class SherdogSpider(CrawlSpider):
         tabs[1::2] = tabs_even
 
         # -- collect all fights
+
         Fights = {}
         i = 1
         for tab in reversed(tabs):
             FI = FightItem()
-            FI["Verdict"] = tab.select(".//span/text()")[0].extract()
-            FI["Opponent"] = tab.select(".//a[@href]/text()")[0].extract()
-            FI["Event"] = tab.select(".//a[@href]//text()")[1].extract()
-            FI["Date"] = dt.datetime.strptime(tab.select(".//span[@class='sub_line']/text()")[0].extract(), "%b / %d / %Y")
-            FI["Method"] = tab.select(".//td/text()")[0].extract()
-            FI["Round"] = int(tab.select(".//td/text()")[1].extract())
-            if tab.select(".//td/text()")[2].extract() != "N/A":
-                if re.search(";", tab.select(".//td/text()")[2].extract()):
-                    FI["Time"] = sum(int(x) * 60 **k for k, x in enumerate(reversed(tab.select(".//td/text()")[2].extract().split(";"))))
+            FI["Verdict"] = tab.xpath(".//span/text()")[0].extract()
+            FI["Opponent"] = tab.xpath(".//a[@href]/text()")[0].extract()
+            FI["Event"] = tab.xpath(".//a[@href]//text()")[1].extract()
+            FI["Date"] = dt.datetime.strptime(tab.xpath(".//span[@class='sub_line']/text()")[0].extract(), "%b / %d / %Y")
+            FI["Method"] = tab.xpath(".//td/text()")[0].extract()
+            FI["Round"] = int(tab.xpath(".//td/text()")[1].extract())
+            if tab.xpath(".//td/text()")[2].extract() != "N/A":
+                if re.search(";", tab.xpath(".//td/text()")[2].extract()):
+                    FI["Time"] = sum(int(x) * 60 **k for k, x in enumerate(reversed(tab.xpath(".//td/text()")[2].extract().split(";"))))
                 else:
-                    FI["Time"] = sum(int(x) * 60 **k for k, x in enumerate(reversed(tab.select(".//td/text()")[2].extract().split(":"))))
+                    FI["Time"] = sum(int(x) * 60 **k for k, x in enumerate(reversed(tab.xpath(".//td/text()")[2].extract().split(":"))))
             else:
                 FI["Time"] = 0
             Fights["Fight" + str(i)] = dict(FI)
             i += 1
 
         # -- Bio of each fighter
-        bio = hxs.select("//div[@class='bio']")
+        bio = hxs.xpath("//div[@class='bio']")
         AI = AttrItem()
-        AI["Name"] = hxs.select(".//span[@class='fn']/text()")[0].extract()
-        if bio.select(".//span[@itemprop='birthDate']/text()")[0].extract() != "N/A":
-            AI["Birthday"] = dt.datetime.strptime(bio.select(".//span[@itemprop='birthDate']/text()")[0].extract(), "%Y-%m-%d")
+        AI["Name"] = hxs.xpath(".//span[@class='fn']/text()")[0].extract()
+        if bio.xpath(".//span[@itemprop='birthDate']/text()")[0].extract() != "N/A":
+            AI["Birthday"] = dt.datetime.strptime(bio.xpath(".//span[@itemprop='birthDate']/text()")[0].extract(), "%Y-%m-%d")
         else:
              AI["Birthday"] = "Unknown"
-        AI["Weight"] = int(bio.select(".//span[@class='item weight']//strong/text()").extract()[0].split()[0])
-        AI["Height"] = sum(int(x) * 12 ** k for k, x in enumerate(reversed(bio.select(".//span[@class='item height']/strong/text()").extract()[0].split("\"")[0].split("\'"))))
-        AI["Class"] = bio.select(".//strong[@class='title']/text()").extract()[0]
-        if bio.select(".//strong[@itemprop='nationality']/text()").extract():
-            AI["Country"] = bio.select(".//strong[@itemprop='nationality']/text()").extract()[0]
+        AI["Weight"] = int(bio.xpath(".//span[@class='item weight']//strong/text()").extract()[0].split()[0])
+        AI["Height"] = sum(int(x) * 12 ** k for k, x in enumerate(reversed(bio.xpath(".//span[@class='item height']/strong/text()").extract()[0].split("\"")[0].split("\'"))))
+        AI["Class"] = bio.xpath(".//strong[@class='title']/text()").extract()[0]
+        if bio.xpath(".//strong[@itemprop='nationality']/text()").extract():
+            AI["Country"] = bio.xpath(".//strong[@itemprop='nationality']/text()").extract()[0]
 
         # -- a nested dict
         Fighter = FighterItem()
